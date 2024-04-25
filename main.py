96b9cb33f5e99e552e3144ff0fa1b4871cd6ecc5
RonaldFerro <rferroguerra@ucsd.edu>
2024-04-24 17:56:44 -0700

    VU meter submission

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Write your code here :-)
+
 import board
 from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
@@ -11,9 +13,16 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26,  # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,  # type: ignore
+    board.IO34,  # type: ignore
+    board.IO48,  # type: ignore
+    board.IO35,  # type: ignore
+    board.IO36,  # type: ignore
+    board.IO37,  # type: ignore
+    board.IO38,  # type: ignore
+    board.IO39,  # type: ignore
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -21,17 +30,20 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+minim = 0
+maxim = 28000
+step = (maxim - minim) / len(led_pins)
+
 # main loop
 while True:
     volume = microphone.value
-
+    pinset = max(volume-20000, 0) // step
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for a in range (0, len(leds)):
+        leds[a].value = False
 
-    sleep(1)
 
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    for a in range (0, pinset):
+        leds[a].value = True
+
